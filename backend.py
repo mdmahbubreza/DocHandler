c52f6cce04b2386c92051ced4c3e910fd515b9d0
Md Mahbub Reza <141657720+mdmahbubreza@users.noreply.github.com>
2025-01-08 21:52:21 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -71,4 +71,4 @@ def download_file(filename):
         return jsonify({'error': 'File not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
